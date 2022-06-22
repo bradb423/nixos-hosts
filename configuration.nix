91b0a5fefc3b1b9f16cbcb38ba07747862b9825a
Bradley Burns <bradleyburns00@gmail.com>
2022-06-22 18:34:02 +0100

    Add neofetch to list of programs

--- configuration.nix
+++ configuration.nix
@@ -113,6 +113,7 @@ in
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
     nano
+    neofetch
     firefox
     htop
   ];
