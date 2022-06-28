fe3b412b24f8bfc129dd8b7b721d0580853e5bcb
Bradley Burns <bradleyburns00@gmail.com>
2022-06-28 18:50:45 +0100

    Add utility to order lists in nixos configs
    Added a small Rust program to order lists within my config files.
    
    Usage:
    
    - `cd list-orderer`
    - `cargo run filepath start_line end_line`
    - Ordered list will then be spat out into the terminal, and can be copied into the config file

---
+++ list-orderer/src/lib.rs
@@ -0,0 +1,48 @@
+/// Struct that represents the arguments of the program.
+pub struct Invocation {
+    pub filename: String,
+    pub start: usize,
+    pub end: usize,
+}
+
+/// Implementation of the `Invocation` struct, creating a new `Invocation`
+/// instance.
+impl Invocation {
+    pub fn new(args: &[String]) -> Result<Invocation, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        let filename = args[1].clone();
+        let start = match args[2].parse::<usize>() {
+            Ok(x) => x,
+            Err(_) => {
+                help();
+                return Err("You must specify a number for the start point");
+            }
+        };
+        let end = match args[3].parse::<usize>() {
+            Ok(x) => x,
+            Err(_) => {
+                help();
+                return Err("You must specify a number for the endpoint");
+            }
+        };
+
+        Ok(Invocation {
+            filename,
+            start,
+            end,
+        })
+    }
+}
+
+pub fn help() {
+    println!(
+        "\n\nThis is a program to order the lists within the configuration files of this repository.
+    To use this script, you must provide three arguments:
+    1: The name of the file you wish to order - String
+    2: The starting line of the list - Integer
+    3: The ending line of the list - Integer\n\n"
+    );
+}
