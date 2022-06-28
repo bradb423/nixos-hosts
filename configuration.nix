a5f4319943fc9031570fa8d5b71030aac308ae94
Bradley Burns <bradleyburns00@gmail.com>
2022-06-28 19:25:50 +0100

    Add command-line utilities to configuration.nix
    Added:
    
    - ripgrep
    - fd
    - bat

--- configuration.nix
+++ configuration.nix
@@ -116,10 +116,13 @@ in
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
+    bat
+    fd
     firefox
     htop
     nano
     neofetch
+    ripgrep
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
