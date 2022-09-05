6908e10251270c452c36ee76010209a7e216acc6
Bradley Burns <bradleyburns00@gmail.com>
2022-09-05 15:51:12 +0100

    Add more information to describe the tor shell

--- shells/tor.nix
+++ shells/tor.nix
@@ -1,3 +1,11 @@
+# There can be issues with creating a shell for Tor when using the stable
+# channel. This is due to the old links for older Tor versions being deleted by
+# default, meaning that tor on the stable channel, which is an older version
+# of Tor, is not available by default.
+
+# The solution to this is to use the unstable channel, provided that the
+# unstable channel has the latest version of Tor.
+
 let
   unstableTarball =
     fetchTarball
@@ -9,6 +17,7 @@ in
 with pkgs;
 
 mkShell {
+  name = "tor-shell";
   buildInputs = [
     unstable.tor-browser-bundle-bin
   ];
