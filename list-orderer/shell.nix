52867df6f33704a895485709a342672727805ee5
Bradley Burns <bradleyburns00@gmail.com>
2024-01-02 18:02:43 +0000

    Add bespoke shell for list-orderer

---
+++ list-orderer/shell.nix
@@ -0,0 +1,24 @@
+let
+  unstable = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz")) {};
+  pkgs = import <nixpkgs> {};
+in
+
+pkgs.mkShell {
+  name = "rust-shell";
+  nativeBuildInputs = with pkgs; [
+    unstable.rustc
+    unstable.cargo
+    unstable.gcc
+    # Some crates may require the following (Such as when building Nushell)
+    # pkg-config
+    # openssl
+  ];
+  buildInputs = with pkgs; [
+    rustfmt
+    clippy
+    cargo-nextest
+    cargo-tarpaulin
+  ];
+
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+}
